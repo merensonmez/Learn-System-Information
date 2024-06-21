8a181d6c1459ff182a46198947ef6435897706f8
Muhammed Eren Sönmez <erensonmez.bussines@gmail.com>
2024-06-22 02:22:13 +0300

    Update SonmezV1.bat

--- SonmezV1.bat
+++ SonmezV1.bat
@@ -1,36 +1,3 @@
-::[Bat To Exe Converter]
-::
-::YAwzoRdxOk+EWAjk
-::fBw5plQjdCyDJGyX8VAjFDdxYiu0E1eeA6YX/Ofr0/OGuOnMWEbudbPY1bqaJe4Hp0jle5cq03dflt9BHwJLahemUV1i+j4a4TzLPsST0w==
-::YAwzuBVtJxjWCl3EqQJgSA==
-::ZR4luwNxJguZRRnk
-::Yhs/ulQjdF+5
-::cxAkpRVqdFKZSzk=
-::cBs/ulQjdF+5
-::ZR41oxFsdFKZSDk=
-::eBoioBt6dFKZSDk=
-::cRo6pxp7LAbNWATEpCI=
-::egkzugNsPRvcWATEpCI=
-::dAsiuh18IRvcCxnZtBJQ
-::cRYluBh/LU+EWAnk
-::YxY4rhs+aU+IeA==
-::cxY6rQJ7JhzQF1fEqQJhZks0
-::ZQ05rAF9IBncCkqN+0xwdVsFAlTi
-::ZQ05rAF9IAHYFVzEqQIDJxVZSR60bQs=
-::eg0/rx1wNQPfEVWB+kM9LVsJDDeNMma/HNU=
-::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
-::cRolqwZ3JBvQF1fEqQJQ
-::dhA7uBVwLU+EWGqL+k81Mns=
-::YQ03rBFzNR3SWATElA==
-::dhAmsQZ3MwfNWATE0VA1JltnQwqPOXHa
-::ZQ0/vhVqMQ3MEVWAtB9wSA==
-::Zg8zqx1/OA3MEVWAtB9wSA==
-::dhA7pRFwIByZRRnk
-::Zh4grVQjdCyDJGyX8VAjFDdxYiu0E1eeA6YX/Ofr09mIrEAYTtRuNorD39Q=
-::YB416Ek+ZG8=
-::
-::
-::978f952a14a936cc963da21a135fa983
 @echo off
 echo Hello Welcome to SonmezV1!!!
 echo     E R E N S O N M E Z
